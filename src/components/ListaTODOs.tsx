7175b8bad623f211f6306a5aa4db449c7201f356
Hernan Quintana <hernan@jsatch.com>
2025-05-08 21:08:51 -0500

    Mostrando listado de TODOs de forma dinamica

--- src/components/ListaTODOs.tsx
+++ src/components/ListaTODOs.tsx
@@ -1,24 +1,25 @@
-const ListaTODOs = () => {
+export interface TODO {
+    id : number,
+    descripcion : string
+}
+
+interface ListaTODOsProps {
+    data : TODO[]
+}
+
+const ListaTODOs = (props : ListaTODOsProps) => {
     return <div className="mt-4">
         <ul className="list-group">
-            <li className="list-group-item d-flex justify-content-between align-items-start">
-                <div className="ms-2 me-auto">
-                    Cocinar el almuerzo
-                </div>
-                <input className="form-check-input" type="checkbox" />
-            </li>
-            <li className="list-group-item d-flex justify-content-between align-items-start">
-                <div className="ms-2 me-auto">
-                    Cocinar el almuerzo
-                </div>
-                <input className="form-check-input" type="checkbox" />
-            </li>
-            <li className="list-group-item d-flex justify-content-between align-items-start">
-                <div className="ms-2 me-auto">
-                    Cocinar el almuerzo
-                </div>
-                <input className="form-check-input" type="checkbox" />
-            </li>
+            {
+                props.data.map( (elemento : TODO) => {
+                    return <li className="list-group-item d-flex justify-content-between align-items-start">
+                        <div className="ms-2 me-auto">
+                            { elemento.descripcion }
+                        </div>
+                        <input className="form-check-input" type="checkbox" />
+                    </li>
+                })
+            }
         </ul>
     </div>
 }
--- src/pages/MainPage.tsx
+++ src/pages/MainPage.tsx
@@ -1,16 +1,21 @@
 import Formulario from "../components/Formulario"
-import ListaTODOs from "../components/ListaTODOs"
+import ListaTODOs, { type TODO } from "../components/ListaTODOs"
 import Navegacion from "../components/Navegacion"
 import Titulo from "../components/Titulo"
 
 const MainPage = () => {
     const titulo = "TODO App"
 
+    const lista : TODO[] = [
+        { id : 1, descripcion : "Ir al cine" },
+        { id : 2, descripcion : "Limpiar la casa"}
+    ]
+
     return <div className="container">
         <Titulo texto={ titulo }/>
         <Navegacion />
         <Formulario />
-        <ListaTODOs />
+        <ListaTODOs data={ lista }/>
     </div>
 }
 
