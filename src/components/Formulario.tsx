8bdb2036882d40f4958f31549b63a1d419192244
ProfO2302 <ProfO2302@lab.aca>
2025-05-12 21:14:23 -0500

    TextoTodo gestionado por variable de estado

--- src/components/Formulario.tsx
+++ src/components/Formulario.tsx
@@ -3,15 +3,26 @@ import { useState } from "react"
 const Formulario = () => {
     const [ textoTODO, setTextoTODO ] = useState<string>("")
 
+    const textoTODOOnChange = (e : React.ChangeEvent<HTMLInputElement>) => {
+        setTextoTODO(e.currentTarget.value)
+    }
+
     return <div className="mt-4">
         <div className="row">
             <div className="col-md-10">
                 <input className="form-control" 
                     type="text"
-                    value={ textoTODO } />
+                    value={ textoTODO }
+                    onChange={ textoTODOOnChange } />
             </div>
             <div className="col-md-2">
-                <button className="btn btn-success" type="button">Agregar</button>
+                <button className="btn btn-success" 
+                    type="button"
+                    onClick={ () => {
+                        console.log("Debe agregar elemento: " + textoTODO)
+                    } }>
+                        Agregar
+                </button>
             </div>
         </div>
     </div>
