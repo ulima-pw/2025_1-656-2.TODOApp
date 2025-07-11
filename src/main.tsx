93364cf4c1be8476230fd048a927d29c528b76a1
Hernan Quintana <hernan@jsatch.com>
2025-07-10 19:37:40 -0500

    Preparando para  subir a la nube

--- src/main.tsx
+++ src/main.tsx
@@ -9,7 +9,7 @@ import { BrowserRouter, Route, Routes } from 'react-router-dom'
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <BrowserRouter>
+    <BrowserRouter basename='/2025_1-656-2.TODOApp'>
       <Routes>
         <Route path="/" element={ <LoginPage /> } />
         <Route path="/main" element={ <MainPage /> } />
